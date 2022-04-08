a2af0a05cfe6028e74e1f5f48125293c7841083b
benjalonso <alonsobenjaminlb@gmail.com>
2022-04-08 15:15:56 +0000

    terminado, de  300 lineas de código pasamos 70

--- src/app.js
+++ src/app.js
@@ -28,100 +28,43 @@ window.onload = function() {
       message
     } = form;
     // Expresiones regulares
-    let onlyText = /^[a-zA-Z\s]+$/;
+    let onlyText = /^[a-zA-Z\s\u00E0-\u00FC]+$/;
     let onlyNumbers = /^[0-9]+$/;
     let onlyNumbersLength = /^[0-9]{16}$/;
     let onlyNumbersLengthToCVC = /^[0-9]{3,4}$/;
-    //El siguiente código nos valida el campo de Card
-    if (card.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      card.style.background = "#F5B7B1";
-      errors = true;
-    } else if (!onlyNumbersLength.test(card.value)) {
-      alert.style.display = "block";
-      alert.innerHTML = "Debe contener 16 dígitos";
-      card.style.background = "#F5B7B1";
-      errors = true;
-    } else (card.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de CVC
-    if (cvc.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      cvc.style.background = "#F5B7B1";
-      errors = true;
-    } else if (!onlyNumbersLengthToCVC.test(cvc.value)) {
-      alert.style.display = "block";
-      alert.innerHTML = "Debe contener 3 a 4 dígitos";
-      cvc.style.background = "#F5B7B1";
-      errors = true;
-    } else (cvc.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de Amount
-    if (amount.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      amount.style.background = "#F5B7B1";
-      errors = true;
-    } else (amount.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de Firstname
-    if (firstname.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      firstname.style.background = "#F5B7B1";
-      errors = true;
-    } else if (!onlyText.test(firstname.value)) {
-      alert.style.display = "block";
-      alert.innerHTML = "Debe contener un nombre";
-      firstname.style.background = "#F5B7B1";
-      errors = true;
-    } else {
-      (firstname.style.background = "none"), (errors = false);
-    }
-    //El siguiente código nos valida el campo de Lastname
-    if (lastname.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      lastname.style.background = "#F5B7B1";
-      errors = true;
-    } else if (!onlyText.test(lastname.value)) {
-      alert.style.display = "block";
-      alert.innerHTML = "Debe contener un apellido";
-      lastname.style.background = "#F5B7B1";
-      errors = true;
-    } else (lastname.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de City
-    if (city.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      city.style.background = "#F5B7B1";
-      errors = true;
-    } else if (!onlyText.test(city.value)) {
-      alert.style.display = "block";
-      alert.innerHTML = "Debe contener una ciudad";
-      city.style.background = "#F5B7B1";
-      errors = true;
-    } else (city.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de State
-    if (state.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      state.style.background = "#F5B7B1";
-      errors = true;
-    } else (state.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de Postal Code
-    if (postalCode.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      postalCode.style.background = "#F5B7B1";
-      errors = true;
-    } else (postalCode.style.background = "none"), (errors = false);
-    //El siguiente código nos valida el campo de Message
-    if (message.value === "") {
-      alert.style.display = "block";
-      alert.innerHTML = "El campo es obligatorio";
-      message.style.background = "#F5B7B1";
-      errors = true;
-    } else (message.style.background = "none"), (errors = false);
     // El siguiente codigo nos alerta sobre los errores en los campos
+    let valoresDeCadaCampo = [
+      card,
+      cvc,
+      amount,
+      firstname,
+      lastname,
+      city,
+      state,
+      postalCode,
+      message
+    ];
+    for (let i = 0; i < valoresDeCadaCampo.length; i++) {
+      if (valoresDeCadaCampo[i].value == "") {
+        valoresDeCadaCampo[i].style.background = "#F5B7B1";
+        alert.style.display = "block";
+        errors = true;
+        alert.innerHTML = "El campo es obligatorio";
+      } else if (!onlyNumbersLength.test(card.value)) {
+        alert.innerHTML = "Debe contener 16 dígitos";
+      } else if (!onlyNumbersLengthToCVC.test(cvc.value)) {
+        alert.innerHTML = "Debe contener 3 a 4 dígitos";
+      } else if (!onlyText.test(firstname.value)) {
+        alert.innerHTML = "Debe contener un nombre";
+      } else if (!onlyText.test(lastname.value)) {
+        alert.innerHTML = "Debe contener un apellido";
+      } else if (!onlyText.test(city.value)) {
+        alert.innerHTML = "Debe contener una ciudad";
+      } else if (!valoresDeCadaCampo[i].value == "") {
+        valoresDeCadaCampo[i].style.background = "white";
+        errors = false;
+        alert.style.display = "none";
+      }
+    }
   });
 };
